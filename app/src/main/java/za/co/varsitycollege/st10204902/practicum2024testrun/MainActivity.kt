d11de7872c9c4cd9d76cbfcbca43b736922aa85a
Nicholas Christopher Meyer <101266733+ST10204902@users.noreply.github.com>
2025-05-27 14:52:16 +0200

    Updates

--- app/src/main/java/za/co/varsitycollege/st10204902/practicum2024testrun/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/practicum2024testrun/MainActivity.kt
@@ -1,8 +1,10 @@
 package za.co.varsitycollege.st10204902.practicum2024testrun
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Variables
+        val goButton = findViewById<AppCompatButton>(R.id.goButton)
+        val exitButton = findViewById<AppCompatButton>(R.id.exitButton)
+
+        //On Click Listener
+        goButton.setOnClickListener {
+            val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+        }
+
+        exitButton.setOnClickListener {
+            finishAffinity() // Close the app
+        }
+
 
     }
 }
